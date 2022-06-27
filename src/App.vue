2d40692378b393b3cd019d12438410f8d3a93d79
Tim Graupmann <tgraupmann@gmail.com>
2022-06-26 19:02:38 -0700

    Added search logic

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,14 @@
     <div style="background: #002; min-width: 500px; min-height: 500px; text-align: left">
       <div>SEARCH:</div>
       <div>
-        <input id="txtSearch" type="text" style="width: 100%" />
+        <input
+          id="txtSearch"
+          type="text"
+          style="width: 100%"
+          @input="filterHandler"
+          @change="filterHandler"
+          :value="search"
+        />
       </div>
       <br />
 
@@ -30,13 +37,19 @@
       </center>
       <div>FILES:</div>
 
-      <div v-for="(file, index) in files" :key="index" style="cursor: pointer; background: #003333; padding: 4px" @click="process(file)">
-        <span v-show="processed(file)"
-          >&nbsp;
-          <u style="color: #0f0" @click="showText(file)">T</u>
-        </span>
-        &nbsp;
-        <u>{{ file }}</u>
+      <div v-for="(file, index) in displayFiles" :key="index">
+        <div
+          v-show="!search || processSearch(file)"
+          style="cursor: pointer; background: #003333; padding: 4px"
+          @click="process(file)"
+        >
+          <span v-show="processed(file)"
+            >&nbsp;
+            <u style="color: #0f0" @click="showText(file)">T</u>
+          </span>
+          &nbsp;
+          <u>{{ file }}</u>
+        </div>
       </div>
     </div>
     <div>
@@ -118,9 +131,9 @@ export default {
                         refThis.autoProcess();
                       }
                       var files = refThis.files;
-                      refThis.files = [];
+                      refThis.displayFiles = [];
                       setTimeout(function () {
-                        refThis.files = files;
+                        refThis.displayFiles = files;
                       }, 0);
                     };
                     if (!refThis.processedKey(json.src)) {
@@ -129,13 +142,14 @@ export default {
                     break;
                   case "readdir":
                     //console.log("json", JSON.stringify(json, null, 2));
+                    this.files = json.files;
                     let localStorage = window.localStorage;
                     if (localStorage) {
                       localStorage.setItem("KEY_OCR_FILES", JSON.stringify(json.files));
                     }
-                    refThis.files = [];
+                    this.displayFiles = [];
                     setTimeout(function () {
-                      refThis.files = json.files;
+                      refThis.displayFiles = json.files;
                     }, 0);
                     break;
                 }
@@ -160,6 +174,7 @@ export default {
       console.log("Scan Files");
       this.autoIndex = -1;
       this.files = [];
+      this.displayFiles = [];
 
       //console.log("streamSocket", this.streamSocket);
       if (!this.streamSocket) {
@@ -202,6 +217,27 @@ export default {
     processed: function (file) {
       let key = this.ocrPath + "\\" + file;
       return this.processedKey(key);
+    },
+    processSearch: function (file) {
+      let key = this.ocrPath + "\\" + file;
+      let localStorage = window.localStorage;
+      if (localStorage) {
+        let text = localStorage.getItem(key);
+        if (text) {
+          return text.toLowerCase().includes(this.search.toLowerCase());
+        }
+      }
+      return false;
+    },
+    filterHandler: function(evt) {
+      this.search = evt.target.value;
+      console.log(this.search);
+      var refThis = this;
+      var files = refThis.files;
+      refThis.displayFiles = [];
+      setTimeout(function() {
+        refThis.displayFiles = files;
+      }, 0);
     },
     showText: function (file) {
       let key = this.ocrPath + "\\" + file;
@@ -266,7 +302,9 @@ export default {
       streamSocket: undefined,
       ocrPath: ocrPath,
       files: files,
+      displayFiles: files,
       autoIndex: -1,
+      search: undefined,
     };
   },
 };
